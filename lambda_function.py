9060651c4d1b024f5286f9dfc05770db270ee4ae
Floodon <39013778+Floodon@users.noreply.github.com>
2023-01-21 16:40:46 +0100

    Added uploading

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ import os
 import pyarrow.parquet as pq 
 import boto3
 from io import BytesIO
-from io import StringIO
 
 #If running on lambda
 #s3 = boto3.client("s3")
@@ -11,38 +10,45 @@ from io import StringIO
 #If running locally
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=<ACCESS KEY>,
-    aws_secret_access_key=<SECRET ACCESS KEY>
+    aws_access_key_id=<AWS_ACCESS_KEY>,
+    aws_secret_access_key=<AWS_SECRET_ACCESS_KEY>
+)
+
+s3_resource = boto3.resource(
+    "s3",
+    aws_access_key_id=<AWS_ACCESS_KEY>,
+    aws_secret_access_key=<AWS_SECRET_ACCESS_KEY>
 )
 
 
 def lambda_handler(event, context):
     #If running on Lambda
-    #bucket_name = event.Records[0].s3.bucket.name
+    #bucket_in = event.Records[0].s3.bucket.name
     #object_key = event.Records[0].object.key
+    #bucket_out = <BUCKET_NAME_OUT>
 
     #If running locally
-    bucket_name = 'bucketamazonscrapper'
-    object_key = '2022_2022_50_20221215_2022-12-15.19.4266.parquet.zst'
+    bucket_in = <BUCKET_NAME_IN>
+    object_key = <OBJECT_KEY_IN>
+    bucket_out = <BUCKET_NAME_OUT>
 
-    file = s3.get_object(Bucket = bucket_name, Key = object_key)
+    file = s3.get_object(Bucket = bucket_in, Key = object_key)
     status = file.get("ResponseMetadata",{}).get("HTTPStatusCode")
 
     if status == 200:
         print("Successful S3 get_object response. Status - {status}")
+
         #Transforming the file into a pandas dataframe
-        fileBody = file['Body'].read()
-        data = BytesIO(fileBody)
+        file_body = file['Body'].read()
+        data = BytesIO(file_body)
         df = pd.read_parquet(data)
+
         #Editing columns
         df = df[['productID','productName','productCategory','productPrice','promotion','productPricePrevious','productRating','productDate','currentURL']]
-        #Uploading the modified file on s3, à tester
-        buffer = StringIO()
-        df.to_parquet(buffer)
-        s3_resource = boto3.resource('s3')
-        s3_resource.Object(bucket_name,'df.parquet').put(Body = buffer.getValue())
+        print("-Successful dataframe cleaning")
 
-    else:
-        print("Unsuccessful S3 get_object response. Status - {status}")
-
-lambda_handler(0,0)
+        #Uploading the modified file to s3
+        buffer = BytesIO()
+        df.to_parquet(path=buffer, compression='None')
+        s3_resource.Object(bucket_out,'df.parquet').put(Body = buffer.getvalue())
+        print("--Successful S3 put_object response. Status - {status}")
